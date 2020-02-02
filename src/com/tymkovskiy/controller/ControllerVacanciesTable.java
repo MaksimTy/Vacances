fd1b8833d9b59279ccfc7a0c355846e4457f55d7
Maksim Tymkovskiy <maksimvt@gmail.com>
2020-02-02 23:42:27 +0300

    TableView added : SimpleVacancyProperty.class, ControllerVacanciesTable.class

--- src/com/tymkovskiy/controller/ControllerVacanciesTable.java
+++ src/com/tymkovskiy/controller/ControllerVacanciesTable.java
@@ -1,50 +1,68 @@
 package com.tymkovskiy.controller;
 
 import com.tymkovskiy.dao.DAOException;
-import com.tymkovskiy.model.Vacancy;
 import com.tymkovskiy.model.VacancyManager;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 
 public class ControllerVacanciesTable {
 
     private VacancyManager vacancyManager;
+    private ObservableList<SimpleVacancyProperty> simpleVacancyProperties;
 
     @FXML
-    private TableView<Vacancy> tableView;
+    private TableView<SimpleVacancyProperty> tableView;
     @FXML
-    private TableColumn<Vacancy, String> id;
+    private TableColumn<SimpleVacancyProperty, Integer> id;
     @FXML
-    private TableColumn<Vacancy, String> company;
+    private TableColumn<SimpleVacancyProperty, String> company;
     @FXML
-    private TableColumn<Vacancy, String> vacancy;
+    private TableColumn<SimpleVacancyProperty, String> vacancy;
     @FXML
-    private TableColumn<Vacancy, String> mail;
+    private TableColumn<SimpleVacancyProperty, String> mail;
     @FXML
-    private TableColumn<Vacancy, String> link;
+    private TableColumn<SimpleVacancyProperty, String> link;
     @FXML
-    private TableColumn<Vacancy, String> dateRequest;
+    private TableColumn<SimpleVacancyProperty, String> dateRequest;
     @FXML
-    private TableColumn<Vacancy, String> answer;
+    private TableColumn<SimpleVacancyProperty, String> answer;
     @FXML
-    private TableColumn<Vacancy, String> dateAnswer;
-    private Object String;
+    private TableColumn<SimpleVacancyProperty, String> dateAnswer;
 
 
-    @FXML
-    public TableView getTableView() throws DAOException {
+    public void initialize() throws DAOException {
 
-        ObservableList<Vacancy> vacancies =
-                FXCollections.observableArrayList(this.vacancyManager.getVacancies());
+        this.getList();
+        this.tableView.setItems(this.simpleVacancyProperties);
+        // определяем фабрику для столбца с привязкой к свойству id
+        this.id.setCellValueFactory(new PropertyValueFactory<>("id"));
+        // определяем фабрику для столбца с привязкой к свойству company
+        this.company.setCellValueFactory(new PropertyValueFactory<>("company"));
+        // определяем фабрику для столбца с привязкой к свойству vacancy
+        this.vacancy.setCellValueFactory(new PropertyValueFactory<>("vacancy"));
+        // определяем фабрику для столбца с привязкой к свойству mail
+        this.mail.setCellValueFactory(new PropertyValueFactory<>("mail"));
+        // определяем фабрику для столбца с привязкой к свойству link
+        this.link.setCellValueFactory(new PropertyValueFactory<>("vacancy_linc"));
+        // определяем фабрику для столбца с привязкой к свойству answer
+        this.answer.setCellValueFactory(new PropertyValueFactory<>("answer"));
+        // определяем фабрику для столбца с привязкой к свойству dateRequest
+        this.dateRequest.setCellValueFactory(new PropertyValueFactory<>("mail_date"));
+        // определяем фабрику для столбца с привязкой к свойству dateAnswer
+        this.dateAnswer.setCellValueFactory(new PropertyValueFactory<>("answer_date"));
+    }
 
-        this.tableView.setItems(vacancies);
-
-
-
-        return tableView;
+    public void getList() throws DAOException {
+        this.vacancyManager = new VacancyManager();
+        this.simpleVacancyProperties = FXCollections.observableArrayList();
+        this.vacancyManager.
+                getVacancies().
+                stream().
+                forEach(x -> this.simpleVacancyProperties.add(new SimpleVacancyProperty(x)));
     }
 }
--- src/com/tymkovskiy/controller/SimpleVacancyProperty.java
+++ src/com/tymkovskiy/controller/SimpleVacancyProperty.java
@@ -1,5 +1,138 @@
 package com.tymkovskiy.controller;
 
 
+import com.tymkovskiy.model.Vacancy;
+import javafx.beans.property.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class SimpleVacancyProperty {
+
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+    /**
+     * id вакансии
+     */
+    private IntegerProperty id;
+    /**
+     * наименование компании
+     */
+    private StringProperty company;
+    /**
+     * наименование вакансии
+     */
+    private StringProperty vacancy;
+    /**
+     * электронный адрес компании
+     */
+    private StringProperty mail;
+    /**
+     * дата отправки резюме
+     */
+    private StringProperty mail_date;
+    /**
+     * текст сопроводительного письма
+     */
+    private StringProperty text;
+    /**
+     * ответ
+     */
+    private StringProperty answer;
+    /**
+     * ссылка на вакансию
+     */
+    private StringProperty vacancy_linc;
+    /**
+     * дата ответа компании
+     */
+    private StringProperty answer_date;
+
+
+    public SimpleVacancyProperty(Vacancy vacancy) {
+        this.id = new SimpleIntegerProperty(vacancy.getId());
+        this.company = new SimpleStringProperty(vacancy.getCompany());
+        this.vacancy = new SimpleStringProperty(vacancy.getVacancy());
+        this.mail = new SimpleStringProperty(vacancy.getMail());
+        this.vacancy_linc = new SimpleStringProperty(vacancy.getVacancy_linc());
+
+        this.mail_date = vacancy.getMail_date() == null ?
+                null : new SimpleStringProperty(sdf.format(vacancy.getMail_date()));
+        this.text = new SimpleStringProperty(vacancy.getText());
+        this.answer = new SimpleStringProperty(vacancy.getAnswer());
+        this.answer_date = vacancy.getAnswer_date() == null ?
+                null : new SimpleStringProperty(sdf.format(vacancy.getAnswer_date()));
+    }
+
+    public int getId() {
+        return id.get();
+    }
+
+    public IntegerProperty idProperty() {
+        return id;
+    }
+
+    public String getCompany() {
+        return company.get();
+    }
+
+    public StringProperty companyProperty() {
+        return company;
+    }
+
+    public String getVacancy() {
+        return vacancy.get();
+    }
+
+    public StringProperty vacancyProperty() {
+        return vacancy;
+    }
+
+    public String getMail() {
+        return mail.get();
+    }
+
+    public StringProperty mailProperty() {
+        return mail;
+    }
+
+    public String getMail_date() {
+        return mail_date.get();
+    }
+
+    public StringProperty mail_dateProperty() {
+        return mail_date;
+    }
+
+    public String getText() {
+        return text.get();
+    }
+
+    public StringProperty textProperty() {
+        return text;
+    }
+
+    public String getAnswer() {
+        return answer.get();
+    }
+
+    public StringProperty answerProperty() {
+        return answer;
+    }
+
+    public String getVacancy_linc() {
+        return vacancy_linc.get();
+    }
+
+    public StringProperty vacancy_lincProperty() {
+        return vacancy_linc;
+    }
+
+    public String getAnswer_date() {
+        return answer_date.get();
+    }
+
+    public StringProperty answer_dateProperty() {
+        return answer_date;
+    }
 }
